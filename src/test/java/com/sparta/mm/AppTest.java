c02bc0d889e9e00a96e4d7dbf0c75eef316d4657
MahamudHub <mo.mahamud@outlook.com>
2020-07-10 12:30:42 +0100

    test commit

--- src/test/java/com/sparta/mm/AppTest.java
+++ src/test/java/com/sparta/mm/AppTest.java
@@ -1,15 +1,11 @@
 package com.sparta.mm;
 
-import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
